124a0718ffe97e5d7fe30c6309c17d87067db107
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-01 16:44:33 -0500

    add indexxeddb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,7 @@
 //import modules
 import "./form.js";
 import "./submit.js";
+import "./database.js"
 
 //import images
 import Logo from '../images/logo.png';
@@ -11,9 +12,11 @@ import Dog from '../images/dog.png';
 import "../css/index.css";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initDb } from "./database.js";
 
 
 window.addEventListener('load', function () {
+    initDb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
