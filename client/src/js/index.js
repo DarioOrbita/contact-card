07048b61d0734847c3a62dfee7422bc23edca02d
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-03 01:39:57 -0500

    add delete card functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,62 +1,71 @@
 //import modules
 import "./submit.js";
-import "./database.js"
+import "./database.js";
 import { initDb } from "./database.js";
-import { initDb, getDb, postDb } from './database';
-import { fetchCards } from './cards';
-import { toggleForm, clearForm } from './form.js';
+import { initDb, getDb, postDb, deleteDb } from "./database";
+import { fetchCards } from "./cards";
+import { toggleForm, clearForm } from "./form.js";
 
 //import images
-import Logo from '../images/logo.png';
-import Bear from '../images/bear.png';
-import Dog from '../images/dog.png';
+import Logo from "../images/logo.png";
+import Bear from "../images/bear.png";
+import Dog from "../images/dog.png";
 
 //import css
 import "../css/index.css";
-import { Tooltip, Toast, Popover } from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-window.addEventListener('load', function() {
+window.addEventListener("load", function () {
   initDb();
   fetchCards();
-  document.getElementById('logo').src = Logo;
-  document.getElementById('bearThumbnail').src = Bear;
-  document.getElementById('dogThumbnail').src = Dog;
+  document.getElementById("logo").src = Logo;
+  document.getElementById("bearThumbnail").src = Bear;
+  document.getElementById("dogThumbnail").src = Dog;
 });
 
-  // Form functionality
-  const form = document.getElementById("formToggle");
-  const newContactButton = document.getElementById("new-contact");
-  let submitBtnToUpdate = false;
-  let profileId;
-  
-  newContactButton.addEventListener('click', event => {
-    toggleForm()
-   })
-  
-  form.addEventListener('submit', event => {
-    // Handle data
-    event.preventDefault();
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener("click", (event) => {
+  toggleForm();
+});
+
+form.addEventListener("submit", (event) => {
+  // Handle data
+  event.preventDefault();
   let name = document.getElementById("name").value;
   let phone = document.getElementById("phone").value;
   let email = document.getElementById("email").value;
   let profile = document.querySelector('input[type="radio"]:checked').value;
-  
-    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
   if (submitBtnToUpdate == false) {
     postDb(name, email, phone, profile);
   } else {
-  
     fetchCards();
-      // Toggles the submit button back to POST functionality
+    // Toggles the submit button back to POST functionality
     submitBtnToUpdate = false;
   }
-  
+
+  window.deleteCard = (e) => {
+    // Grabs the id from the button element attached to the contact card.
+    let id = parseInt(e.id);
+    // Delete the card
+    deleteDb(id);
+    // Reload the DOM
+    fetchCards();
+  };
+
+
+
   // Clear form
   clearForm();
   // Toggle form
   toggleForm();
   // Reload the DOM
   fetchCards();
-  });
+});
