908476db8c0dafc74697ddbf3d5d6755ff26278e
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-03 01:58:14 -0500

    add edit card functionaltiy

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,7 +2,7 @@
 import "./submit.js";
 import "./database.js";
 import { initDb } from "./database.js";
-import { initDb, getDb, postDb, deleteDb } from "./database";
+import { initDb, getDb, postDb, deleteDb, editDb } from "./database";
 import { fetchCards } from "./cards";
 import { toggleForm, clearForm } from "./form.js";
 
@@ -58,6 +58,25 @@ form.addEventListener("submit", (event) => {
     deleteDb(id);
     // Reload the DOM
     fetchCards();
+  };
+
+  window.editCard = (e) => {
+    // Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element.
+    profileId = parseInt(e.dataset.id);
+  
+    // Grabs information to pre-populate edit form
+    let editName = e.dataset.name;
+    let editEmail = e.dataset.email;
+    let editPhone = e.dataset.phone;
+  
+    document.getElementById("name").value = editName;
+    document.getElementById("email").value = editEmail;
+    document.getElementById("phone").value = editPhone;
+  
+    form.style.display = "block";
+  
+    // Toggles the Submit button so that it now Updates an existing contact instead of posting a new one
+      submitBtnToUpdate = true;
   };
 
 
